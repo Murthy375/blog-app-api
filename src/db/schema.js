fe6e5fcb64abfe64c3b305492602062c2182afaf
Sridhar Murthy K Chandra <sridharkc375@gmail.com>
2026-01-22 20:53:08 +0530

    chore(server): initial database setup with drizzle and postgres

---
+++ src/db/schema.js
@@ -0,0 +1,12 @@
+import { uuid, pgTable, varchar, text, date } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid().primaryKey().defaultRandom(),
+  email: varchar({ length: 256 }).notNull().unique(),
+  name: varchar({ length: 256 }).notNull().unique(),
+  password: text().notNull(),
+  fname: varchar({ length: 256 }).notNull(),
+  lname: varchar({ length: 256 }).notNull(),
+  joinedOn: date().notNull(),
+  salt: text().notNull(),
+});

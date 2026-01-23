bf71b796cb1874adf7880c27527b6a4d5e1b9488
Sridhar Murthy K Chandra <sridharkc375@gmail.com>
2026-01-23 18:47:22 +0530

    feat(db): update schema for usersTable (add mname)

--- src/db/schema.js
+++ src/db/schema.js
@@ -6,6 +6,7 @@ export const usersTable = pgTable("users", {
   name: varchar({ length: 256 }).notNull().unique(),
   password: text().notNull(),
   fname: varchar({ length: 256 }).notNull(),
+  mname: varchar({ length: 256 }).notNull(),
   lname: varchar({ length: 256 }).notNull(),
   joinedOn: date().notNull(),
   salt: text().notNull(),

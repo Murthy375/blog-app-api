09b76c9b41dc894872cfecdc0340588e086425e7
Sridhar Murthy K Chandra <sridharkc375@gmail.com>
2026-01-23 17:04:37 +0530

    feat(feed): add test `/feed` route

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require("express");
+// express setup
+import express from "express";
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
+// router related
+import feedRouter from "./src/routes/feed.routes.js";
+
+
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "server running successfully!" });
+  return res.redirect("http://localhost:8000/feed");
 });
 
+// feed router
+app.use("/feed", feedRouter);
+
 app.listen(PORT, () => {
-  console.log(`server successfully listening @ http://localhost:${PORT}`);
+  console.log(`\nserver successfully listening @ http://localhost:${PORT}`);
 });

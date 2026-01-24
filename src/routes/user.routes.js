d15faf19d98a3e56200f61030106168db5f46e62
Sridhar Murthy K Chandra <sridharkc375@gmail.com>
2026-01-24 22:25:21 +0530

    feat(login): add login route with validation and JWT

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,9 +11,12 @@ import { eq } from "drizzle-orm";
 // crypto related(for passward encryption)
 import { randomBytes, createHmac } from "node:crypto";
 
+// jwt related
+import jwt from "jsonwebtoken";
+
 // all user routes
 router.post("/register", async (req, res) => {
-  // registers new user
+  // registers new user //
 
   const { fname, mname, lname, name, email, password } = req.body;
 
@@ -68,6 +71,68 @@ router.post("/register", async (req, res) => {
     });
 
   return res.status(201).json({ data: userInfo.id });
+});
+
+router.post("/login", async (req, res) => {
+  // logs-in user //
+
+  const { email, password } = req.body;
+
+  if (!email || !password?.trim()) {
+    return res.status(400).json({ error: "bad request: invalid credentials" });
+  }
+
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ error: "bad request: password must be atleast 6 charecters" });
+  }
+
+  const [existingUser] = await db
+    .select({
+      id: usersTable.id,
+      name: usersTable.name,
+      joinedOn: usersTable.joinedOn,
+      salt: usersTable.salt,
+      password: usersTable.password,
+    })
+    .from(usersTable)
+    .where(eq(usersTable.email, email));
+
+  if (!existingUser) {
+    return res.status(409).json({
+      error: `user with email ${existingUser.email} does not exist, register before logging-in`,
+      redirect: "/user/login",
+    });
+  }
+
+  // creating a salt for hashing user-provided password
+  // const salt = randomBytes(256).toString("hex");
+
+  // hash the user-provided password
+  const newHashedPassword = createHmac("sha256", existingUser.salt)
+    .update(password)
+    .digest("hex");
+
+  // console.log(newHashedPassword)
+  // console.log(existingUser.password)
+    
+  if (newHashedPassword !== existingUser.password) {
+    return res.status(401).json({ error: `incorrect passsword` });
+  }
+
+  // payload fof creating a jwt-token
+  const payload = {
+    id: existingUser.id,
+    name: existingUser.name,
+    joinedOn: existingUser.joinedOn,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: "60m",
+  });
+
+  return res.status(200).json({ status: "success", message: `log-in successful`, token: token });
 });
 
 export default router;

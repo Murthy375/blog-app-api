9a7e8a7b9c01b8560f578c1efc3f1dd936a986e5
Sridhar Murthy K Chandra <sridharkc375@gmail.com>
2026-01-30 21:28:24 +0530

    feat(feed): add feed completely to application

--- src/controllers/feed.controller.js
+++ src/controllers/feed.controller.js
@@ -1,3 +1,26 @@
-export const showFeedForUser = function(req, res) {
-    return res.status(200).json({ message: "all blogs on the platform go here" });
-}
+// db related
+import db from "../db/index.js";
+import { usersTable } from "../models/user.model.js";
+import { blogsTable } from "../models/blog.model.js";
+import { eq } from "drizzle-orm";
+
+export const showFeedForUser = async function (req, res) {
+  // shows feed to anyone who comes into the websiete //
+
+  const feed = await db
+    .select({
+      createdAt: blogsTable.createdAt,
+      title: blogsTable.title,
+      tags: blogsTable.tags,
+      content: blogsTable.content,
+      // author info
+      fname: usersTable.fname,
+      mname: usersTable.mname,
+      lname: usersTable.lname,
+      usersName: usersTable.name,
+    })
+    .from(blogsTable)
+    .innerJoin(usersTable, eq(blogsTable.userId, usersTable.id));
+
+  return res.status(200).json({ data: feed });
+};
